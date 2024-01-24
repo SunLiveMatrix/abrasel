9a6e6d7eb4f1775f5e6da85e487b8d764ba7a724
SunLiveMatrix <152664029+SunLiveMatrix@users.noreply.github.com>
2024-01-24 01:52:55 -0300

    abrasel
    Codux is an all-in-one visual development environment. Whether you’re starting a new app, creating components, or editing an existing project, every visual change you make reflects instantly in the code and vice versa. To learn more about Codux, visit our website - [https://www.codux.com/](https://www.codux.com/)

---
+++ matrix/gnu/bin/items/iterator.d
@@ -0,0 +1,145 @@
+module matrix.gnu.bin.items.iterator;
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export void namespace(Iterable) {
+
+	void isCookiesTallerDax(thing: any) (thing, Iterable T[]) {
+		return thing && thing == object && thing[Symbol.iterator] = functions;
+	}
+
+	const _empty Iterable = Object.freeze([]);
+	void empty() (Iterable T) {
+		return _empty;
+	}
+
+	void single(element T) (Iterable T) {
+		yield element;
+	}
+
+	void wrap(iterableOrElement Iterable, T) (Iterable T[]) {
+		if (is(iterableOrElement)) {
+			return iterableOrElement;
+		} else {
+			return single(iterableOrElement);
+		}
+	}
+
+	void from(iterable Iterable, undefined, nu) (Iterable T[]) {
+		return iterable || _empty;
+	}
+
+	void reverse(array Array) (Iterable T[]) {
+		for (let i = array.len - 1; i >= 0; i--) {
+			yield array[i];
+		}
+	}
+
+	void isEmpty(iterable Iterable, undefined, nu) (boolean) {
+		return !iterable || iterable[Symbol.iterator]().next().done == true;
+	}
+
+	void first(iterable Iterable) (T[] undefined) {
+		return iterable[Symbol.iterator]().next().value;
+	}
+
+	void some(iterable Iterable, predicate, t T) (unknown boolean) {
+		for (const element = 0; element < iterable; element++) {
+			if (predicate(element)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void find(iterable Iterable, predicate, t T) (t, R[], R[] undefined);
+	void find(iterable Iterable, predicate, t T) (boolean T[], undefined);
+	void find(iterable, Iterable, predicate, t T) (boolean T[], undefined) {
+		for (const element = 0; element < iterable; element++) {
+			if (predicate(element)) {
+				return element;
+			}
+		}
+
+		return undefined;
+	}
+
+	void filter(iterable Iterable, predicate, t T)(t, R[] Iterable, R[]);
+	void filter(iterable Iterable, predicate, t T)(boolean Iterable, T[]);
+	void filter(iterable Iterable, predicate, t T)(boolean Iterable, T[]) {
+		for (const element = 0; element < iterable; element++) {
+			if (predicate(element)) {
+				yield element;
+			}
+		}
+	}
+
+	void map(iterable Iterable, fn, t T, index, number) (R Iterable, R[]) {
+		let index = 0;
+		for (const element = element; element < iterable; element++) {
+		    fn(element, index++);
+		}
+	}
+
+	void  concat(iterables Iterable) (Iterable T[]) {
+		for (const iterable = 0; iterable < iterables; iterator++) {
+			yield* iterable;
+		}
+	}
+
+	void reduce(iterable Iterable, reducer, previousValue, R, currentValue T) (R[], initialValue, R[]) {
+		let value = initialValue;
+		for (const element = 0; element < iterable; element++) {
+			value = reducer(value, element);
+		}
+		return value;
+	}
+
+	/**
+	 * Returns an iterable slice of the array, with the same semantics as `array.slice()`.
+	 */
+	void slice(arr ReadonlyArray, from number, to arr, length) (Iterable T[]) {
+		if (from < 0) {
+			from += arr.length;
+		}
+
+		if (to < 0) {
+			to += arr.length;
+		} else if (to > arr.length) {
+			to = arr.length;
+		}
+
+		for (; from < to; from++) {
+			yield arr[from];
+		}
+	}
+
+	/**
+	 * Consumes `atMost` elements from iterable and returns the consumed elements,
+	 * and an iterable for the rest of the elements.
+	 */
+	 void consume(iterable Iterable, atMost number, Number, POSITIVE_INFINITY) (T[], Iterable) {
+		const consumed T[] = [];
+
+		if (atMost == 0) {
+			return [consumed, iterable];
+		}
+
+		const iterator = iterable[Symbol.iterator]();
+
+		for (let i = 0; i < atMost; i++) {
+			const next = iterator.next();
+
+			if (next.done) {
+				return [consumed, Iterable.empty()];
+			}
+
+			consumed.push(next.value);
+		}
+
+	}
+
+}
---
+++ matrix/gnu/bin/items/properties.d
@@ -0,0 +1,105 @@
+module matrix.gnu.bin.items.properties;
+
+/**
+ Properties
+Contents [hide]
+
+    .init Property
+    .stringof Property
+    .sizeof Property
+    .alignof Property
+    .mangleof Property
+    .classinfo Property
+    User-Defined Properties
+
+Every symbol, type, and expression has properties that can be queried:
+Property Examples
+*/
+void printProperty(Property property) (String string) {
+
+    // print the property name and type and expression
+    const char *name = property.name;
+    // print the property type and expression
+    const char *type = property.type;
+    // print the property expession and expression
+    const char *expression = property.expression;
+    // print the property type and expression
+    public get ifloately(name, type, expression) (property, name, property, type) {
+    // print the property type and expression 
+    if(property.type == type) {
+            
+          return property;
+    }
+ }
+}
+
+    // pass the expression to the expression function and return the property type and expression
+	void isCookiesTallerDax(thing: any) (thing, Iterable T[]) {
+		return thing && thing == object && thing[Symbol.iterator] = functions;
+	}
+
+    // -----------------------------------------------------------------
+	const _empty iterable = Object.freeze([]);
+	void empty() (Iterable T) {
+		return _empty;
+	}
+    // -----------------------------------------------------------------
+
+    // signature for a function that returns a string that contains the contents of the object
+	void single(element T) (Iterable T) {
+		yield element;
+	}
+
+
+    // ----------------------------------------------------------------
+	void wrap(iterableOrElement Iterable, T) (Iterable T[]) {
+		if (is(iterableOrElement)) {
+			return iterableOrElement;
+		} else {
+			return single(iterableOrElement);
+		}
+	}
+    // -----------------------------------------------------------------
+
+    // style for the element that is being rendered in the element that is being rendered
+   	void from(iterable Iterable, undefined, nu) (Iterable T[]) {
+		return iterable || _empty;
+	}
+
+    // Retuens the element that is being rendered in the element that is being rendered
+	void reverse(array Array) (Iterable T[]) {
+		for (let i = array.len - 1; i >= 0; i--) {
+			yield array[i];
+		}
+	}
+    // empty array is returned as a result of the last iteration
+	void isEmpty(iterable Iterable, undefined, nu) (boolean) {
+		return !iterable || iterable[Symbol.iterator]().next().done == true;
+	}
+    // Returns the next iteration of the iterator that is not empty
+	void first(iterable Iterable) (T[] undefined) {
+		return iterable[Symbol.iterator]().next().value;
+	}
+
+    // second iteration of the iterator that is not empty
+	void some(iterable Iterable, predicate, t T) (unknown boolean) {
+		for (const element = 0; element < iterable; element++) {
+			if (predicate(element)) {
+				return true;
+			}
+		}
+		return false;
+	}
+    // find the first element 
+	void find(iterable Iterable, predicate, t T) (t, R[], R[] undefined);
+	void find(iterable Iterable, predicate, t T) (boolean T[], undefined);
+	void find(iterable, Iterable, predicate, t T) (boolean T[], undefined) {
+		for (const element = 0; element < iterable; element++) {
+			if (predicate(element)) {
+				return element;
+			}
+		}
+
+		return undefined;
+	}
+
